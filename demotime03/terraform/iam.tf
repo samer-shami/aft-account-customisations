8c2d462413d993d4182c20655c1546bca27e73ab
Samer Shami <gitkraken@shogun.work>
2022-04-14 11:23:08 +1000

    add demotime03

---
+++ demotime03/terraform/iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "this" {
+  name = "AccountCustomisationForDemo"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Purpose = "Created through Account Customisations"
+  }
+}
